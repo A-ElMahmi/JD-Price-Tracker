011eaa345128d5b9aa8e8e31e99b00a98b236e23
Abbderrahmane El Mahmi <elmahmi.a04@gmail.com>
2020-07-30 17:47:11 +0100

    Some tidying up

--- src/main.py
+++ src/main.py
@@ -1,13 +1,9 @@
 import requests
 import bs4
 
-# url = "https://www.jdsports.ie/men/mens-clothing/track-pants/"
-url = "https://www.jdsports.ie/men/"
-agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
-response = requests.get(url, headers=agent)
+url = "https://www.jdsports.ie/men/mens-clothing/track-pants/"
+# agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
+response = requests.get(url)
 page = bs4.BeautifulSoup(response.content, "html5lib")
 
-print(page)
-
-for tag in page.findAll("script"):
-  print(tag.get("type"))
+print(page.title)
